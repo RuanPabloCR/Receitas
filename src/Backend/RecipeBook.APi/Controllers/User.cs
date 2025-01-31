2028469f2dec5778d0431e7b275cca1e555e12d8
Ruan Pablo <ruanpablocutrimramon@gmail.com>
2025-01-31 15:00:45 -0300

    feat: adding end point to user an user

---
+++ src/Backend/RecipeBook.APi/Controllers/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RecipeBook.Communication.Requests;
+using RecipeBook.Communication.Responses;
+namespace RecipeBook.APi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class User : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson),StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request)
+        {
+            return Created();
+        }   
+    }
+}
